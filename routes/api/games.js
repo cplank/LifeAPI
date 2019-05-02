47e137ebae99c0c770de4f6364472fed35d1cfa7
cplank <cplankshepard@gmail.com>
2019-05-02 12:55:40 -0700

    added basic routes

--- routes/api/games.js
+++ routes/api/games.js
@@ -1,4 +1,13 @@
 const router = require("express").Router();
 const gameController = require("../../controllers/gameController");
 
-router.route()
+router.route("/")
+    .get(gameController.findAll)
+    .post(gameController.create);
+
+router.route("/:id")
+    .get(gameController.findById)
+    .put(gameController.update)
+    .delete(gameController.remove);
+
+module.exports = router;
