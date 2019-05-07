883426155a5751ff59ca7f76d5e9b669f0d7608f
James Dizon <dizonjerlin@gmail.com>
2019-05-07 09:57:04 -0700

    Updated Files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const path = require("path")
 const router = require("express").Router()
 const gameRoutes = require("./games")
+// const loginRoute = require('../models/auth')
 
 router.use("/games", gameRoutes)
+// router.use('/auth', loginRoute)
 
 module.exports = router;
